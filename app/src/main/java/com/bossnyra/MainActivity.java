d2d64e8d91746f4448886b25b4d4916abbcbf0b2
Boss <boss@example.com>
2026-07-19 17:25:23 +0530

    Start button working

--- app/src/main/java/com/bossnyra/MainActivity.java
+++ app/src/main/java/com/bossnyra/MainActivity.java
@@ -2,6 +2,8 @@ package com.bossnyra;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(v -> {
+            Toast.makeText(this, "BossNyra Chat Started!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
